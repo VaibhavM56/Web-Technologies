cce469218a154498728a09c64103cf05ffe605f2
Vaibhav Mishra <mishravaibhav033@gmail.com>
2022-08-15 20:25:25 +0530

    JavaScript Object

---
+++ JavaScript/objects.js
@@ -0,0 +1,21 @@
+var name = "Vaibhav";
+
+// there are two ways you can create your objects
+// objects are the collection of the key and value pairs
+// 1 -->
+
+var obj = {
+  name: "Vaibhav",
+  PhNo: 9876542103,
+  address: "Bangalore",
+};
+
+console.log(obj);
+
+// 2-->
+
+var obj1 = new Object(); // creating object using new key word
+obj1.name = "Vanity";
+obj1.PhNo = 7894563210;
+obj1.age = 20;
+console.log(obj1);
